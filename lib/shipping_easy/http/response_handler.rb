38e3dd22ffb99d27911c5074e21cfcd5c40c65a2
Theo Mills <twmills@twmills.com>
2014-03-03 12:35:11 -0600

    Add response handler and spec.

---
+++ lib/shipping_easy/http/response_handler.rb
@@ -0,0 +1,27 @@
+class ShippingEasy::Http::ResponseHandler
+
+  extend Forwardable
+
+  def_delegators :response, :status, :body
+
+  attr_reader :response
+
+  def initialize(response)
+    @response = response
+  end
+
+  def self.run(response)
+    new(response).run
+  end
+
+  def run
+    case status
+      when 401 then raise ShippingEasy::AccessDeniedError, response.body
+      when 404 then raise ShippingEasy::ResourceNotFoundError, response.body
+      when 200 then JSON.parse(response.body)
+      else
+        raise ShippingEasy::Error, response.body
+    end
+  end
+
+end
---
+++ spec/http/response_handler_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+describe ShippingEasy::Http::ResponseHandler do
+
+  let(:status) { 200 }
+  let(:body) { { "order_number" => "12345" }}
+  let(:response) do
+    double("response",
+            status: status,
+            body: body.to_json)
+  end
+
+  subject { ShippingEasy::Http::ResponseHandler.new(response) }
+
+  describe "#run" do
+    context "when success" do
+      specify { subject.run.should == body }
+    end
+    context "when authentication fails" do
+      let(:status) { 401 }
+      specify { expect { subject.run }.to raise_error(ShippingEasy::AccessDeniedError) }
+    end
+    context "when resource cannot be found" do
+      let(:status) { 404 }
+      specify { expect { subject.run }.to raise_error(ShippingEasy::ResourceNotFoundError) }
+    end
+    context "when unexpected error occurs" do
+      let(:status) { 500 }
+      specify { expect { subject.run }.to raise_error(ShippingEasy::Error) }
+    end
+  end
+
+end
