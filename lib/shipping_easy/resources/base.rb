33553e94ef28410fafc0f2824f34ace5dd7ff1c9
Theo Mills <twmills@twmills.com>
2014-05-20 14:41:16 -0500

    Handle http params better.

--- lib/shipping_easy/resources/base.rb
+++ lib/shipping_easy/resources/base.rb
@@ -1,10 +1,13 @@
 class ShippingEasy::Resources::Base
 
-  def self.command(name, command_options, &block)
+  def self.command(name, command_options = {}, &block)
     define_singleton_method name do |options = {}|
-      options[:relative_path] = command_options.fetch(:relative_path, block.call(options))
-      options[:http_method] = command_options.fetch(:http_method, :get)
-      execute_request!(options)
+      request_options = {}
+      request_options[:relative_path] = command_options.delete(:relative_path) || block.call(options)
+      request_options[:http_method] = command_options.delete(:http_method) || :get
+      request_options[:payload] = options.delete(:payload) if options.has_key?(:payload)
+      request_options[:params] = options unless options.nil? || options.empty?
+      execute_request!(request_options)
     end
   end
 
--- spec/resources/base_spec.rb
+++ spec/resources/base_spec.rb
@@ -12,24 +12,33 @@ describe ShippingEasy::Resources::Base do
     end
 
     it "extracts options to send to a request" do
+      GenericResource.command(:find_all, http_method: :get) do |args|
+        "/orders"
+      end
+      GenericResource.should_receive(:execute_request!).with({:relative_path=>"/orders",
+                                                              :http_method=>:get,
+                                                              :params => {:page => 2,
+                                                                          :per_page => 3,
+                                                                          :status => [:shipped]}})
 
+      GenericResource.find_all(:page => 2, :per_page => 3, :status => [:shipped] )
     end
 
     context "when a block is provided" do
       it "uses it as the value for the path" do
-        GenericResource.command(:create, method: :post) do |args|
+        GenericResource.command(:create, http_method: :post) do |args|
           "/this/is/the/path"
         end
-        GenericResource.should_receive(:execute_request!).with({:relative_path=>"/this/is/the/path", :http_method=>:get})
+        GenericResource.should_receive(:execute_request!).with({:relative_path=>"/this/is/the/path", :http_method=>:post})
         GenericResource.create
       end
 
       context "and an argument is passed in" do
         it "interpolates it" do
-          GenericResource.command(:create, method: :post) do |args|
+          GenericResource.command(:create, http_method: :post) do |args|
             "/this/is/the/#{args.delete(:name)}"
           end
-          GenericResource.should_receive(:execute_request!).with({:relative_path=>"/this/is/the/ABC123", :http_method=>:get})
+          GenericResource.should_receive(:execute_request!).with({:relative_path=>"/this/is/the/ABC123", :http_method=>:post})
           GenericResource.create(name: "ABC123")
         end
       end
