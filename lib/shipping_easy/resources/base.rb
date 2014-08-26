f23eebb5deaf3ef15816d5aa6ddea3d0eae194fc
Theo Mills <twmills@twmills.com>
2014-08-26 08:46:23 -0500

    Toggle partner vs. public API requests.

--- lib/shipping_easy/resources/base.rb
+++ lib/shipping_easy/resources/base.rb
@@ -7,12 +7,13 @@ class ShippingEasy::Resources::Base
       request_options[:http_method] = command_options.fetch(:http_method, :get)
       request_options[:payload] = options.delete(:payload) if options.has_key?(:payload)
       request_options[:params] = options unless options.nil? || options.empty?
-      execute_request!(request_options)
+      execute_request!(request_options, command_options.fetch(:request_type, :public))
     end
   end
 
-  def self.execute_request!(options = {})
-    response = ShippingEasy::Http::Request.connect!(options)
+  def self.execute_request!(options = {}, request_type)
+    request_class = request_type == :partner ? ShippingEasy::Http::PartnerRequest : ShippingEasy::Http::Request
+    response = request_class.connect!(options)
     ShippingEasy::Http::ResponseHandler.run(response)
   end
 
