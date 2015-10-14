c62cf67eb79d367c9221a1fbed070f98b3463360
Theo Mills <twmills@twmills.com>
2015-10-14 15:31:10 -0500

    Add update status and recipient calls.

--- spec/resources/order_spec.rb
+++ spec/resources/order_spec.rb
@@ -5,7 +5,7 @@ describe ShippingEasy::Resources::Order do
   describe ".find_all" do
     context "when store ID is not included" do
       it "sends a request with the expected options" do
-        ShippingEasy::Resources::Order.should_receive(:execute_request!).with({ :relative_path => "/orders",
+        expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/orders",
                                                                                 :http_method => :get,
                                                                                 :params => { :page => 2,
                                                                                              :per_page => 3,
@@ -17,7 +17,7 @@ describe ShippingEasy::Resources::Order do
 
     context "when store ID is included" do
       it "sends a request with the expected options" do
-        ShippingEasy::Resources::Order.should_receive(:execute_request!).with({ :relative_path => "/stores/123456/orders",
+        expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/stores/123456/orders",
                                                                                 :http_method => :get,
                                                                                 :params => { :page => 2,
                                                                                              :per_page => 3,
@@ -31,14 +31,14 @@ describe ShippingEasy::Resources::Order do
   describe ".find" do
     context "when store ID is not included" do
       it "sends a request with the expected options" do
-        ShippingEasy::Resources::Order.should_receive(:execute_request!).with({ :relative_path => "/orders/2", :http_method => :get }, :public)
+        expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/orders/2", :http_method => :get }, :public)
         ShippingEasy::Resources::Order.find(:id => 2)
       end
     end
 
     context "when store ID is included" do
       it "sends a request with the expected options" do
-        ShippingEasy::Resources::Order.should_receive(:execute_request!).with({ :relative_path => "/stores/123456/orders/2", :http_method => :get }, :public)
+        expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/stores/123456/orders/2", :http_method => :get }, :public)
         ShippingEasy::Resources::Order.find(:store_api_key => "123456", :id => 2)
       end
     end
@@ -46,8 +46,30 @@ describe ShippingEasy::Resources::Order do
 
   describe ".create" do
     it "sends a request with the expected options" do
-      ShippingEasy::Resources::Order.should_receive(:execute_request!).with({ :relative_path => "/stores/123456/orders", :http_method => :post, payload: { name: "Jack" } }, :public)
+      expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/stores/123456/orders", :http_method => :post, payload: { name: "Jack" } }, :public)
       ShippingEasy::Resources::Order.create(:store_api_key => "123456", payload: { name: "Jack" })
+    end
+  end
+
+  describe ".update_recipient" do
+    let(:payload) do
+      { recipient: { first_name: "Jack" } }
+    end
+
+    it "sends a request with the expected options" do
+      expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/stores/123456/orders/2/recipient", :http_method => :put, payload: payload}, :public)
+      ShippingEasy::Resources::Order.update_recipient(:store_api_key => "123456", id: 2, payload: payload)
+    end
+  end
+
+  describe ".change_status" do
+    let(:payload) do
+      { order: { status: "shipped" } }
+    end
+
+    it "sends a request with the expected options" do
+      expect(ShippingEasy::Resources::Order).to receive(:execute_request!).with({ :relative_path => "/stores/123456/orders/2/status", :http_method => :put, payload: payload}, :public)
+      ShippingEasy::Resources::Order.update_status(:store_api_key => "123456", id: 2, payload: payload)
     end
   end
 end
