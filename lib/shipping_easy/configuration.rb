64db7c1ee4fe3b6dc665c47c0dcb99a6515e7caa
Carl Brasic <cbrasic@gmail.com>
2016-05-31 12:26:17 -0500

    Update base url to new default, ignoring the old default

--- lib/shipping_easy/configuration.rb
+++ lib/shipping_easy/configuration.rb
@@ -13,19 +13,29 @@
 #
 module ShippingEasy
   class Configuration
+    LEGACY_URL = "https://app.shippingeasy.com"
+    DEFAULT_URL = "https://api.shippingeasy.com"
 
+    attr_reader   :base_url
     attr_accessor :api_key,
                   :api_secret,
                   :partner_api_key,
                   :partner_api_secret,
                   :api_version,
-                  :base_url,
                   :http_adapter
 
     # Creates a configuration object, setting the default attributes.
     def initialize
       @http_adapter = ShippingEasy::Http::FaradayAdapter
-      @base_url = "https://app.shippingeasy.com"
+      @base_url = DEFAULT_URL
+    end
+
+    def base_url=(val)
+      if val == LEGACY_URL
+        @base_url = DEFAULT_URL
+      else
+        @base_url = val
+      end
     end
   end
 end
